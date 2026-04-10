55fb806fe700abba3a572a6c3a276a6a65b173d9
Andrew Kuklewicz <andrew@beginsinwonder.com>
2026-04-10 12:59:29 -0400

    Upgrade rollup 1.x to 4.x, replace closure compiler with terser
    - Upgrade rollup ^1.27.14 -> ^4.0.0
    - Upgrade @rollup/plugin-commonjs ^11.0.0 -> ^29.0.0
    - Upgrade @rollup/plugin-node-resolve ^6.0.0 -> ^16.0.0
    - Replace @ampproject/rollup-plugin-closure-compiler with @rollup/plugin-terser
    - Remove ES3 class check from test loader (no longer transpiling to ES3)
    - Bundle build time reduced from ~4.5s to ~450ms

--- test/loader.js
+++ test/loader.js
@@ -19,9 +19,6 @@ let wavefile;
 if (findArg('--umd')) {
 	console.log('umd tests');
 	wavefile = require('../dist/wavefile.js').WaveFile;
-	if (wavefile.toString().slice(0, 5) === "class") {
-		throw new Error('WaveFile in UMD dist should not be a ES6 class.');
-	}
 
 // Source
 } else {
